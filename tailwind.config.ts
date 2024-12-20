5e25d90cc35895ba5999f3579acccb9b4f56f92e
Nguyễn Văn Tùng <tungnv@eway.vn>
2024-12-20 16:35:48 +0700

    Add ui library shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,21 @@
 import type { Config } from "tailwindcss";
+import twAnimate from "tailwindcss-animate";
 
 export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
-  plugins: [],
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
+  },
+  plugins: [twAnimate],
 } satisfies Config;
